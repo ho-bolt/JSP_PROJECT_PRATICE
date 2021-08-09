f9baf64998dcfe08d8c3a92d7ad325abf4caf140
ho-bolt <gbolt@naver.com>
2021-08-09 17:46:18 +0900

    게시판 목록 dao및 화면

--- BBS/src/bbs/BbsDAO.java
+++ BBS/src/bbs/BbsDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BbsDAO{
 	private Connection conn;
@@ -73,5 +74,47 @@ public int getNext() {
 			e.printStackTrace();
 		}
 		return -1;//데이터 베이스 오류 
+	}
+	
+	public ArrayList<Bbs> getList(int pageNumber){
+		
+		String sql="SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable=1 ORDER BY bbsID DESC LIMIT 10";
+		ArrayList<Bbs> list=new ArrayList<Bbs>();
+		try {
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, getNext()-(pageNumber-1)*10);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				Bbs bbs=new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				list.add(bbs);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list; //데이터 베이스 오류 
+	}
+	
+	public boolean nextPage(int pageNumber) {
+
+		String sql="SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable=1 order by bbsID desc limit 10 ";
+		try {
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, getNext()-(pageNumber-1)*10);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false; //데이터 베이스 오류 
 	}
 }
